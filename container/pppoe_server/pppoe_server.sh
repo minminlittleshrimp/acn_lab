defeeec3d052a99f4543d9aec8b1ce277e3a41e5
Requiem <public_receiver035@pm.me>
2024-10-28 12:57:35 +0700

    scaled to 4 clients

--- container/pppoe_server/pppoe_server.sh
+++ container/pppoe_server/pppoe_server.sh
@@ -5,10 +5,21 @@ cat <<EOL > /etc/network/interfaces
 auto lo
 iface lo inet loopback
 
-auto eth2
-iface eth2 inet static
-address 192.168.1.254
-netmask 255.255.255.0
+auto e1-1
+iface e1-1 inet static
+	address 20.20.20.1/24
+
+auto e1-2
+iface e1-2 inet static
+	address 20.20.20.1/24
+
+auto e1-3
+iface e1-3 inet static
+	address 20.20.20.1/24
+
+auto e1-4
+iface e1-4 inet static
+	address 20.20.20.1/24
 EOL
 
 # DNS server
@@ -35,19 +46,29 @@ EOL
 
 # pap setup
 cat <<EOL > /etc/ppp/pap-secrets
-user1       *    pass1       *
-user2       *    pass2       *
+"user1"	*	"pass1" *
+"user2"	*	"pass2"	*
+"user3"	*	"pass3"	*
+"user4"	*	"pass4"	*
+"user5"	*	"pass5"	*
 EOL
 
 # ip range
 cat <<EOL > /etc/ppp/allip
-192.168.1.1-50
+20.20.20.11-99
 EOL
 
-# pppoe server
-#pppoe-server -I eth1 -F
-#pppoe-server -C isp -L 192.168.1.254 -p /etc/ppp/allip -I eth2 -F
-
 # Set up IP forwarding
-#echo 1 > /proc/sys/net/ipv4/ip_forward
-#iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o eth1 -j MASQUERADE
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -s 20.20.20.0/24 -o eth0 -j MASQUERADE
+
+# Restart networking
+/etc/init.d/networking stop
+/etc/init.d/networking start
+
+# PPPoE Server
+pppoe-server -C isp -L 20.20.20.1 -p /etc/ppp/allip -I e1-1 -F &
+pppoe-server -C isp -L 20.20.20.1 -p /etc/ppp/allip -I e1-2 -F &
+pppoe-server -C isp -L 20.20.20.1 -p /etc/ppp/allip -I e1-3 -F &
+pppoe-server -C isp -L 20.20.20.1 -p /etc/ppp/allip -I e1-4 -F &
+
